7f0b1e584a1fd2fb2d102a5846a2e4295422dd0a
Tiny-666 <55028485+Tiny-666@users.noreply.github.com>
2019-10-27 23:46:52 +0800

    优化了出牌算法

--- 13water/src/CHUPAI.java
+++ 13water/src/CHUPAI.java
@@ -193,121 +193,150 @@ public class CHUPAI {
 	public static String Get_Gourd_Pair(int tri,int j)//取出葫芦的一对，末尾不带空格
 	{
 		String Bottom="";
-		int count=0,fk=0,mh=0,ht=0,heit=0,max=-1;
-		if(fangkuai[j]==1)
+		int count=0,fk=0,mh=0,ht=0,heit=0,max=-1,max2=-1,str_exist=0,str_destroy=-1,th_exist=0;
+		if(shoupai[j]==2)
 		{
-			if((fangkuai[tri]==0&&fangkuai[13]!=5)||(fangkuai[tri]==1&&fangkuai[13]!=6))
+			for(int i=0;i<13;i++)//判断原本有没有顺子
 			{
-				fangkuai[j]=0;
-				fangkuai[13]-=1;
-				Bottom+="#"+(char)(j+50)+" ";
-				fk=1;
-				count++;
-			}
-		}
-		if(meihua[j]==1)
-		{
-			if((meihua[tri]==0&&meihua[13]!=5)||(meihua[tri]==1&&meihua[13]!=6))
-			{
-				meihua[j]=0;
-				meihua[13]-=1;
-				Bottom+="*"+(char)(j+50);
-				mh=1;
-				if(count<1)
-					Bottom+=" ";
-				else
-				{
-					shoupai[j]-=2;
-					return Bottom;
-				}
-				count++;
-			}
-		}
-		if(hongtao[j]==1)
-		{
-			if((hongtao[tri]==0&&hongtao[13]!=5)||(hongtao[tri]==1&&hongtao[13]!=6))
-			{
-				hongtao[j]=0;
-				hongtao[13]-=1;
-				Bottom+="&"+(char)(j+50);
-				ht=1;
-				if(count<1)
-					Bottom+=" ";
-				else
-				{
-					shoupai[j]-=2;
-					return Bottom;
-				}
-				count++;
-			}
-		}
-		if(heitao[j]==1)
-		{
-			if((heitao[tri]==0&&heitao[13]!=5)||(heitao[tri]==1&&heitao[13]!=6))
-			{
-				heitao[j]=0;
-				heitao[13]-=1;
-				Bottom+="$"+(char)(j+50);
-				heit=1;
-				if(count<1)
-					Bottom+=" ";
-				else
-				{
-					shoupai[j]-=2;
-					return Bottom;
-				}
-				count++;
-			}
-		}
-		//没破坏或者根本没有同花时，会在上面结束
-		if(count==1)//只找到单张，需要还原回去(破坏了同花) 	  //1.shoupai[j]=2,单纯的对子,其中一张破坏了同花
-															  //2.shoupai[j]=3,破坏了两个同花（不可能）
-		{
-			if(fk==1)
-			{
-				fangkuai[j]=1;
-				fangkuai[13]+=1;
-			}
-			else if(mh==1)//只需要还原一张，故可以加上else来加快速度
-			{
-				meihua[j]=1;
-				meihua[13]+=1;
-			}
-			else if(ht==1)
-			{
-				hongtao[j]=1;
-				hongtao[13]+=1;
-			}
-			else if(heit==1)
-			{
-				heitao[j]=1;
-				heitao[13]+=1;
-			}
-		}
-		count=0;
-		for(int i=0;i<13;i++)//判断有没有其他顺子
-		{
-			if(shoupai[i]>0)
-			{
-				if(i!=tri&&i!=j)
+				if(shoupai[i]>0)
 				{
 					count++;
-					if(max<count)
-						max=count;
+					if(max2<count)
+						max2=count;
 				}
 				else
 					count=0;
 			}
+			if(max2>=5)//原本有顺子
+				str_exist=1;
+			count=0;
+			for(int i=0;i<13;i++)//判断有没有其他顺子
+			{
+				if(shoupai[i]>0)
+				{
+					if(i!=tri&&i!=j)
+					{
+						count++;
+						if(max<count)
+							max=count;
+					}
+					else
+						count=0;
+				}
+				else
+					count=0;
+			}
+			if(max>=5)//有其他顺子，可以拿出来
+				str_destroy=0;
 			else
-				count=0;
+				str_destroy=1;
 		}
-		if(max>=5)//有其他顺子，可以拿出来
+		//判断是否存在其他同花
+		count=0;
+		if((fangkuai[tri]==0&&fangkuai[13]>4)||(fangkuai[tri]==1&&fangkuai[13]>5))
+			th_exist=1;
+		else if((meihua[tri]==0&&meihua[13]>4)||(meihua[tri]==1&&meihua[13]>5))
+			th_exist=1;
+		else if((hongtao[tri]==0&&hongtao[13]>4)||(hongtao[tri]==1&&hongtao[13]>5))
+			th_exist=1;
+		else if((heitao[tri]==0&&heitao[13]>4)||(heitao[tri]==1&&heitao[13]>5))
+			th_exist=1;
+		if(th_exist==1)//存在其他同花
 		{
-			if(shoupai[j]==2)//只是个对子，直接拿出来
-				return GetAPair(j);
+			if(fangkuai[j]==1)
+			{
+				if((fangkuai[tri]==0&&fangkuai[13]!=5)||(fangkuai[tri]==1&&fangkuai[13]!=6))
+				{
+					fangkuai[j]=0;
+					fangkuai[13]-=1;
+					Bottom+="#"+(char)(j+50)+" ";
+					fk=1;
+					count++;
+				}
+			}
+			if(meihua[j]==1)
+			{
+				if((meihua[tri]==0&&meihua[13]!=5)||(meihua[tri]==1&&meihua[13]!=6))
+				{
+					meihua[j]=0;
+					meihua[13]-=1;
+					Bottom+="*"+(char)(j+50);
+					mh=1;
+					if(count<1)
+						Bottom+=" ";
+					else
+					{
+						shoupai[j]-=2;
+						return Bottom;
+					}
+					count++;
+				}
+			}
+			if(hongtao[j]==1)
+			{
+				if((hongtao[tri]==0&&hongtao[13]!=5)||(hongtao[tri]==1&&hongtao[13]!=6))
+				{
+					hongtao[j]=0;
+					hongtao[13]-=1;
+					Bottom+="&"+(char)(j+50);
+					ht=1;
+					if(count<1)
+						Bottom+=" ";
+					else
+					{
+						shoupai[j]-=2;
+						return Bottom;
+					}
+					count++;
+				}
+			}
+			if(heitao[j]==1)
+			{
+				if((heitao[tri]==0&&heitao[13]!=5)||(heitao[tri]==1&&heitao[13]!=6))
+				{
+					heitao[j]=0;
+					heitao[13]-=1;
+					Bottom+="$"+(char)(j+50);
+					heit=1;
+					if(count<1)
+						Bottom+=" ";
+					else
+					{
+						shoupai[j]-=2;
+						return Bottom;
+					}
+					count++;
+				}
+			}
+			//没破坏同花时，会在上面结束
+			if(count==1)//只找到单张，需要还原回去(破坏了同花) 	  //1.shoupai[j]=2,单纯的对子,其中一张破坏了同花
+																  //2.shoupai[j]=3,破坏了两个同花（不可能）
+			{
+				if(fk==1)
+				{
+					fangkuai[j]=1;
+					fangkuai[13]+=1;
+				}
+				else if(mh==1)//只需要还原一张，故可以加上else来加快速度
+				{
+					meihua[j]=1;
+					meihua[13]+=1;
+				}
+				else if(ht==1)
+				{
+					hongtao[j]=1;
+					hongtao[13]+=1;
+				}
+				else if(heit==1)
+				{
+					heitao[j]=1;
+					heitao[13]+=1;
+				}
+			}
 		}
-		//没有其他顺子
-		
+		//没有其他同花，或者取出时同花被破坏
+			if((th_exist==0&&str_exist==0)||str_destroy==0)//顺子没被破坏，或者不存在顺子和同花
+				return GetAPair(j);
 		return "NULL";
 	}
 	public static String GetATri(int i)//取出三条，末尾不带空格
@@ -390,35 +419,74 @@ public class CHUPAI {
 		}
 		return "NULL";
 	}
-	public static String TH(int[] arr,String huase)//取出单花色中最大的同花，并返回
+	public static String TH(int[] arr,String huase,String dun)//取出单花色中最大的同花，并返回
 	{
 		StringBuffer str=new StringBuffer();
 		str.insert(0, "\"");
-		int count=0,CouNT=arr[13];
-		//System.out.println(CouNT);
+		int count=0,CouNT=arr[13],divide_pair=0,c1=-1,c2=-1,c3=-1,c4=-1,c5=-1;
 		for(int i=12;i>=0;i--)
 		{
 			if(arr[i]>0)
 			{
-				if(CouNT-5>0&&shoupai[i]>1)
+				if(CouNT-5>0&&shoupai[i]>1)//绕过对子,从大的对子开始绕
 					CouNT--;
 				else
 				{
-					arr[i]-=1;
-					arr[13]-=1;
-					shoupai[i]-=1;
+					if(shoupai[i]==2)
+					{
+						divide_pair++;
+						if(divide_pair==2)//拆了两个对子
+						{
+							if(fangkuai[14]!=arr[14]&&fangkuai[13]>4);//有其他的同花
+							else if(meihua[14]!=arr[14]&&meihua[13]>4);
+							else if(hongtao[14]!=arr[14]&&hongtao[13]>4);
+							else if(heitao[14]!=arr[14]&&heitao[13]>4);
+							if(dun=="Bottom")//底墩
+							{
+								if(fangkuai[14]==arr[14]&&fangkuai[13]>9);//自身有10张同花及以上
+								else if(meihua[14]==arr[14]&&meihua[13]>9);
+								else if(hongtao[14]==arr[14]&&hongtao[13]>9);
+								else if(heitao[14]==arr[14]&&heitao[13]>9);
+								else//都没有 
+									break;
+							}
+							else//中墩
+							{
+								if(fangkuai[14]==arr[14]&&fangkuai[13]>4);//自身有5张同花及以上
+								else if(meihua[14]==arr[14]&&meihua[13]>4);
+								else if(hongtao[14]==arr[14]&&hongtao[13]>4);
+								else if(heitao[14]==arr[14]&&heitao[13]>4);
+								else//都么有
+									break;
+							}
+						}
+					}
 					str.insert(0, huase+(char)(i+50));
 					count++;
+					if(count==1) c1=i;
+					else if(count==2) c2=i;
+					else if(count==3) c3=i;
+					else if(count==4) c4=i;
+					else if(count==5) c5=i;
 					if(count<5)
 						str.insert(0, " ");
 					else
-						break;
+					{
+						arr[c1]=arr[c2]=arr[c3]=arr[c4]=arr[c5]=0;
+						arr[13]-=5;
+						shoupai[c1]-=1;
+						shoupai[c2]-=1;
+						shoupai[c3]-=1;
+						shoupai[c4]-=1;
+						shoupai[c5]-=1;
+						str.insert(0, "\"");
+						String Str=str.toString();
+						return Str;
+					}
 				}
 			}
 		}
-		str.insert(0, "\"");
-		String Str=str.toString();
-		return Str;
+		return "NULL";
 	}
 	public static String Judge_THS()//判断整个手牌中有没有同花顺,有则取出，没有返回“NULL”
 	{
@@ -459,14 +527,63 @@ public class CHUPAI {
 		}
 		return "NULL";
 	}
-	public static String Judge_Gourd()//判断整个手牌中有没有葫芦，有则取出，没有返回“NULL”
+	public static String Judge_Gourd(String dun)//判断整个手牌中有没有葫芦，有则取出，没有返回“NULL”
 	{
 		String Bottom="",Pair="";
 		int trible=Trible();
-		int link=Link_Double();
+		int link=Link_Double(),pair=0,tri=0;
 		if(trible!=-1)//有三张相同
 		{
-			//System.out.println(link);
+			if(dun=="Bottom")//是底墩
+			{
+				for(int i=0;i<=12;i++)
+				{
+					if(shoupai[i]==2)//判断还剩下多少对
+						pair++;
+					else if(shoupai[i]==3&&i!=trible)//判断除了自己以外还有没有三条
+						tri++;
+				}
+				if(tri<1&&pair<3)
+				{//判断有没有其他顺子、同花
+					if(fangkuai[trible]>0&&fangkuai[13]>5);
+					else if(meihua[trible]>0&&meihua[13]>5);
+					else if(hongtao[trible]>0&&hongtao[13]>5);
+					else if(heitao[trible]>0&&heitao[13]>5);
+					else if(meihua[trible]==0&&meihua[13]>5);
+					else if(hongtao[trible]==0&&hongtao[13]>5);
+					else if(heitao[trible]==0&&heitao[13]>5);
+					else if(fangkuai[trible]==0&&fangkuai[13]>5);
+					else//没有其他同花,找其他顺子
+					{
+						int i;
+						for(i=0;i<8;i++)
+						{
+							if(shoupai[i]>0&&shoupai[i+1]>0&&shoupai[i+2]>0&&shoupai[i+3]>0&&shoupai[i+4]>0)
+							{
+								if(i<=trible&&i+4>=trible)
+									continue;
+								else//找到其他顺子
+									break;
+							}
+						}
+						if(i==9)//没找到
+							return "NULL";
+					}
+				}
+			}
+			else//是中墩
+			{
+				int p=0;
+				for(int i=0;i<=12;i++)
+				{
+					if(shoupai[i]==2)//找最大对子
+						p=i;
+					else if(shoupai[i]==3&&i!=trible)//判断除了自己以外还有没有三条
+						tri++;
+				}
+				if(p>7||tri>0)
+					return "NULL";
+			}
 			if(link==-1)//没有连对
 			{
 				for(int j=0;j<=12;j++)//直接找剩下的最小的对子，若没能返回，说明没有能凑成葫芦的牌
@@ -480,24 +597,25 @@ public class CHUPAI {
 								Bottom+="\""+GetATri(trible)+" "+Pair+"\"";
 								else//对子牌面比三条牌面小
 								Bottom+="\""+Pair+" "+GetATri(trible)+"\"";
-								//System.out.println(Arrays.toString(shoupai));
 								return Bottom;
 						}
 					}
 				}
-				for(int j=0;j<=12;j++)//直接找剩下的最小的对子，若没能返回，说明没有能凑成葫芦的牌
+				if(dun!="Middle")//不是中墩
 				{
-					if(shoupai[j]>2&&j!=trible)//找到可以凑成葫芦的牌
+					for(int j=0;j<=12;j++)//直接找剩下的最小的对子，若没能返回，说明没有能凑成葫芦的牌
 					{
-						Pair=Get_Gourd_Pair(trible,j);
-						//System.out.println(Pair);
-						if(Pair!="NULL")
+						if(shoupai[j]>2&&j!=trible)//找到可以凑成葫芦的牌
 						{
-							if(j>trible)//对子牌面比三条牌面大
-								Bottom+="\""+GetATri(trible)+" "+Pair+"\"";
-								else//对子牌面比三条牌面小
-								Bottom+="\""+Pair+" "+GetATri(trible)+"\"";
-								return Bottom;
+							Pair=Get_Gourd_Pair(trible,j);
+							if(Pair!="NULL")
+							{
+								if(j>trible)//对子牌面比三条牌面大
+									Bottom+="\""+GetATri(trible)+" "+Pair+"\"";
+									else//对子牌面比三条牌面小
+									Bottom+="\""+Pair+" "+GetATri(trible)+"\"";
+									return Bottom;
+							}
 						}
 					}
 				}
@@ -552,7 +670,7 @@ public class CHUPAI {
 		}
 		return "NULL";
 	}
-	public static String Judge_TH()//判断整个手牌中有没有同花，有则取出，没有返回“NULL”
+	public static String Judge_TH(String dun)//判断整个手牌中有没有同花，有则取出，没有返回“NULL”
 	{
 		String Bottom="NULL";
 		int i;
@@ -562,40 +680,85 @@ public class CHUPAI {
 			{
 				if(fangkuai[13]>4&&fangkuai[i]>0)
 				{
-					Bottom=TH(fangkuai,"#");
+					Bottom=TH(fangkuai,"#",dun);
+					if(Bottom!="NULL")
 					break;
 				}
-				else if(meihua[13]>4&&meihua[i]>0)
+				if(meihua[13]>4&&meihua[i]>0)
 				{
-					Bottom=TH(meihua,"*");
+					Bottom=TH(meihua,"*",dun);
+					if(Bottom!="NULL")
 					break;
 				}
-				else if(hongtao[13]>4&&hongtao[i]>0)
+				if(hongtao[13]>4&&hongtao[i]>0)
 				{
-					Bottom=TH(hongtao,"&");
+					Bottom=TH(hongtao,"&",dun);
+					if(Bottom!="NULL")
 					break;
 				}
-				else if(heitao[13]>4&&heitao[i]>0)
+				if(heitao[13]>4&&heitao[i]>0)
 				{
-					Bottom=TH(heitao,"$");
+					Bottom=TH(heitao,"$",dun);
+					if(Bottom!="NULL")
 					break;
 				}
 			}
 		}
 		return Bottom;
 	}
-	public static String Judge_Str()//判断整个手牌中有没有顺子，有则取出，没有返回“NULL”
+	public static String Judge_Str(String dun)//判断整个手牌中有没有顺子，有则取出，没有返回“NULL”
 	{
 		String Bottom="\"";
+		int CouNT=0,max=-1,pos=-1;
+		for(int i=0;i<13;i++)
+		{
+			if(shoupai[i]>0)//查找最长连续串
+			{
+				CouNT++;
+				if(CouNT>max)
+				{
+					max=CouNT;
+					pos=i;
+				}
+			}
+			else
+				CouNT=0;
+		}
+		if(max<5)//最大长度<5，没有顺子
+			return "NULL";
 		for(int i=8;i>=0;i--)
 		{
 			if(shoupai[i]>0&&shoupai[i+1]>0&&shoupai[i+2]>0&&shoupai[i+3]>0&&shoupai[i+4]>0)
 			{
+				if(i<=pos)
+				{
+					if(max>5&&(shoupai[i]==2||shoupai[i+1]==2||shoupai[i+2]==2||shoupai[i+3]==2||shoupai[i+4]==2))
+					{//能跳的大对子尽量跳
+						if(i>0)
+							if(shoupai[i-1]==1)//避免破坏前面的对子
+							{
+								max--;
+								continue;
+							}
+					}
+				}
+				if(dun=="Middle")//是中墩
+				{
+					if(max>5&&(shoupai[i]==1||shoupai[i+1]==1||shoupai[i+2]==1||shoupai[i+3]==1||shoupai[i+4]==1))
+					{//能跳的单牌尽量跳
+						if(i>0)
+							if(shoupai[i-1]==1)//避免破坏前面的对子
+							{
+								max--;
+								continue;
+							}
+					}
+				}
 				Bottom+=GetACard(i)+" ";
 				Bottom+=GetACard(i+1)+" ";
 				Bottom+=GetACard(i+2)+" ";
 				Bottom+=GetACard(i+3)+" ";
-				Bottom+=GetACard(i+4)+"\"";
+				Bottom+=GetACard(i+4)+"\"ST";
 				return Bottom;
 			}
 		}
@@ -653,11 +816,11 @@ public class CHUPAI {
 		}
 		return Bottom;
 	}
-	public static String Judge_TwoP(String st)//判断整个手牌中有没有两对（包括连对），有则取出，没有返回“NULL”
+	public static String Judge_TwoP(String dun)//判断整个手牌中有没有两对（包括连对），有则取出，没有返回“NULL”
 	{
 		String Bottom="";
 		int link=Link_Double();
-		int p1=-1,p2=-1,p3=-1,p4=-1,p5=-1,count=0;
+		int p1=-1,p2=-1,p3=-1,p4=-1,p5=-1,p6=-1,count=0;
 		for(int i=12;i>=0;i--)//找对子，从大的开始
 		{
 			if(shoupai[i]==2)
@@ -673,11 +836,13 @@ public class CHUPAI {
 					p4=i;
 				else if(count==5)
 					p5=i;
+				else if(count==6)
+					p6=i;
 			}
 		}
 		if(count<2)//没找到两对
 			return "NULL";
-		if(st=="Bottom")//是底墩
+		if(dun=="Bottom")//是底墩
 		{
 			if(p4==-1)//只有两对或三对，要分到前一墩一对
 				Bottom=Judge_Pair();
@@ -686,7 +851,14 @@ public class CHUPAI {
 				if(link!=-1)//存在连对
 					Bottom=GetTwoPairs(link+1,link);
 				else//不存在连对
-					Bottom=GetTwoPairs(p1,p2);
+				{
+					if(p5==-1)//四对
+						Bottom=GetTwoPairs(p1,p4);
+					else if(p6==-1)//五对
+						Bottom=GetTwoPairs(p1,p5);
+					else//六对
+						Bottom=GetTwoPairs(p1,p6);
+				}
 			}
 		}
 		else//是中墩
@@ -698,7 +870,12 @@ public class CHUPAI {
 				if(link!=-1)//存在连对
 					Bottom=GetTwoPairs(link+1,link);
 				else//不存在连对
-					Bottom=GetTwoPairs(p1,p2);
+				{
+					if(p4==-1)//三对
+					Bottom=GetTwoPairs(p2,p3);
+					else//四对
+					Bottom=GetTwoPairs(p3,p4);
+				}
 			}
 		}
 		return Bottom;
@@ -802,21 +979,21 @@ public class CHUPAI {
 			return str;
 		}
 		//判断葫芦
-		if((str=Judge_Gourd())!="NULL")
+		if((str=Judge_Gourd(dun))!="NULL")
 		{
 			//System.out.println(Arrays.toString(shoupai));
 			return str;
 		}
 			
 		//判断同花
-		if((str=Judge_TH())!="NULL")
+		if((str=Judge_TH(dun))!="NULL")
 		{
 			//System.out.println(Arrays.toString(shoupai));
 			str+="TH";
 			return str;
 		}
 		//判断顺子
-		if((str=Judge_Str())!="NULL")
+		if((str=Judge_Str(dun))!="NULL")
 		{
 			//System.out.println(Arrays.toString(shoupai));
 			return str;
@@ -849,8 +1026,7 @@ public class CHUPAI {
 	}
 	public String CardPlaying(String str) 
 	{
-		/**/
-		//String str="#7 &2 &K #5 $7 $10 *8 $J &9 *K &6 #J $K";
+		//String str="*4 #4 $3 *8 *9 *3 &K #8 *J &4 #A #10 *Q";
 		System.out.println(str);
 		String Result ="";
 		String Top ="";
@@ -939,65 +1115,69 @@ public class CHUPAI {
 				}
 			}
 		}
-		if(Bottom.length()>16&&Middle.length()>16)//出现了双同花,可能会反水
+		if(Bottom.length()>16&&Middle.length()>16)//两墩出现顺子+顺子/同花+同花/同花+顺子的情况,可能会反水
 		{
-			int[] a=new int[13];
-			int[] b=new int[13];
+			int[] a=new int[5];
+			int[] b=new int[5];
 			int cnt=0;
-			Bottom=Bottom.replace("\"", "");//去除全部引号
-			Middle=Middle.replace("\"", "");//去除全部引号
-			Bottom=Bottom.replaceAll("TH", "");
-			Middle=Middle.replaceAll("TH", "");
-			Trans(Bottom);
-			for(int i=12;i>=0;i--)
+			if(Bottom.charAt(16)<Middle.charAt(16))//根据标志字符串判断,字典序T在S后面，故TH>ST
 			{
-				if(shoupai[i]==1)
-				{
-					a[cnt++]=i;
-					shoupai[i]=0;
-				}
+				String temp=Middle;
+				Middle=Bottom;
+				Bottom=temp;
 			}
-			cnt=0;
-			Trans(Middle);
-			for(int i=12;i>=0;i--)
+			else
 			{
-				if(shoupai[i]==1)
-				{
-					b[cnt++]=i;
-					shoupai[i]=0;
-				}
-			}
-			for(int i=0;i<5;i++)
-			{
-				if(b[i]>a[i])//b数组中有比a数组大的元素，即中墩大于底墩时，换位
-				{
-					String temp=Bottom;
-					Bottom="\""+Middle+"\"";
-					Middle="\""+temp+"\"";
-					break;
-				}
-				else if(b[i]<a[i])
-				{
-					Bottom="\""+Bottom+"\"";
-					Middle="\""+Middle+"\"";
-					break;
-				}
+				Bottom=Bottom.replaceAll("TH", "");
+				Bottom=Bottom.replaceAll("ST", "");
+				Middle=Middle.replaceAll("TH", "");
+				Middle=Middle.replaceAll("ST", "");
+				Bottom=Bottom.replaceAll("\"", "");
+				Middle=Middle.replaceAll("\"", "");
+				Trans(Bottom);
+				for(int i=12;i>=0;i--)
+					if(shoupai[i]>0)
+					{
+						a[cnt++]=i;
+						shoupai[i]=0;
+						if(cnt==5)
+							break;
+					}
+				Trans(Middle);
+				for(int i=12;i>=0;i--)
+					if(shoupai[i]>0)
+					{
+						b[cnt++]=i;
+						if(cnt==5)
+							break;
+					}
+				//数组a存Bottom的牌面值，b存Middle的牌面值
+				for(int i=0;i<5;i++)
+					if(b[i]>a[i])
+					{
+						String temp=Middle;
+						Middle=Bottom;
+						Bottom=temp;
+						break;
+					}
+					else if(a[i]>b[i])
+						break;
 			}
 		}
-		else if(Bottom.length()>16)//单同花
-			Bottom=Bottom.replaceAll("TH", "");
-		else if(Middle.length()>16)//单同花
-			Middle=Middle.replaceAll("TH", "");
 		Top=Top.replaceAll(":", "10");
 		Top=Top.replaceAll(";", "J");
 		Top=Top.replaceAll("<", "Q");
 		Top=Top.replaceAll("=", "K");
 		Top=Top.replaceAll(">", "A");
+		Bottom=Bottom.replaceAll("TH", "");
+		Bottom=Bottom.replaceAll("ST", "");
 		Bottom=Bottom.replaceAll(":", "10");
 		Bottom=Bottom.replaceAll(";", "J");
 		Bottom=Bottom.replaceAll("<", "Q");
 		Bottom=Bottom.replaceAll("=", "K");
 		Bottom=Bottom.replaceAll(">", "A");
+		Middle=Middle.replaceAll("TH", "");
+		Middle=Middle.replaceAll("ST", "");
 		Middle=Middle.replaceAll(":", "10");
 		Middle=Middle.replaceAll(";", "J");
 		Middle=Middle.replaceAll("<", "Q");
